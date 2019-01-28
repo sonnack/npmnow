f940fcf0cd5864136ac71a01177e5189febe1254
Sonnack Shawn <ss39512@wdxss39512td8.90.deere.com>
2019-01-28 13:53:15 -0600

    Back to lambda

--- index.js
+++ index.js
@@ -1,9 +1,4 @@
-const http = require('http');
-
-let app = http.createServer((req, res) => {
+module.exports = (req, res) => {
     console.log('Look I ran!')
     res.end(`Hello from Node.js on Now 2.0!`);
-});
-
-app.listen(3000, '127.0.0.1');
-console.log('Node server running on port 3000');
+};
