87324c2b85ef389d1c4650214e66a93bcb9ade98
Sonnack Shawn <ss39512@wdxss39512td8.90.deere.com>
2019-01-28 08:49:40 -0600

    Trying to create a server

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-module.exports = (req, res) => {
+const http = require('http');
+
+let app = http.createServer((req, res) => {
+    console.log('Look I ran!')
     res.end(`Hello from Node.js on Now 2.0!`);
-};
+});
+
+app.listen(3000, '127.0.0.1');
+console.log('Node server running on port 3000');
